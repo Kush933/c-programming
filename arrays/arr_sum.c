84eb5f2c39fa2b270fc5b878d1076800195c433c
Khushboo <khushkri97@gmail.com>
2026-03-30 17:53:57 +0530

    Add files via upload

---
+++ arrays/arr_sum.c
@@ -0,0 +1,16 @@
+//sum of all elements in an array
+#include<stdio.h>
+int main(){
+    int n,arr[100],sum=0;
+    printf("enter the number of elements in an array");
+    scanf("%d",&n);
+    printf("enter the elements of the array");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+        
+
+    }
+    printf("sum is %d \n",sum);
+    return 0;
+}
